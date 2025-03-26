998a5e4477916143a47420073152dec376dfa629
Hikakkun <htrb0130@icloud.com>
2025-03-26 21:30:11 +0900

    ruff format

--- src/api_endpoints.py
+++ src/api_endpoints.py
@@ -5,7 +5,8 @@ from fastapi import FastAPI, HTTPException, status
 from nature_locak_api_client import NatureLocalAPIClient
 import requests
 
-def create_or_update_ir_signal(engine : Engine ,name : str, ir_signal : IRSignal):
+
+def create_or_update_ir_signal(engine: Engine, name: str, ir_signal: IRSignal):
     with Session(engine) as session:
         ir_signal_table = session.get(IRSignalTable, name)
         if ir_signal_table:
@@ -22,27 +23,39 @@ def create_or_update_ir_signal(engine : Engine ,name : str, ir_signal : IRSignal
             session.commit()
             session.refresh(new_ir_signal_table)
             return {"message": f"IR signal '{name}' created successfully."}
-    
-def delete_ir_signal(engine : Engine, name : str):
+
+
+def delete_ir_signal(engine: Engine, name: str):
     with Session(engine) as session:
         ir_signal_table = session.get(IRSignalTable, name)
         if ir_signal_table:
             session.delete(ir_signal_table)
             session.commit()
         else:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"IR signal with name '{name}' not found.")    
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"IR signal with name '{name}' not found.",
+            )
 
-def get_ir_signal(engine : Engine, name : str) -> IRSignal:
+
+def get_ir_signal(engine: Engine, name: str) -> IRSignal:
     with Session(engine) as session:
         ir_signal_table = session.get(IRSignalTable, name)
         if ir_signal_table:
             return ir_signal_table.convert_ir_signal()
         else:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"IR signal with name '{name}' not found.")    
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"IR signal with name '{name}' not found.",
+            )
 
-def send_ir_signal(engine : Engine, name : str, client : NatureLocalAPIClient):
+
+def send_ir_signal(engine: Engine, name: str, client: NatureLocalAPIClient):
     if client is None:
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Nature Remo IP address not configured.")        
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Nature Remo IP address not configured.",
+        )
 
     with Session(engine) as session:
         ir_signal_table = session.get(IRSignalTable, name)
@@ -50,23 +63,30 @@ def send_ir_signal(engine : Engine, name : str, client : NatureLocalAPIClient):
             try:
                 client.send_ir_signal(ir_signal_table.convert_ir_signal())
             except requests.exceptions.RequestException as e:
-                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error sending IR signal to Nature Remo: {e}")
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Error sending IR signal to Nature Remo: {e}",
+                )
         else:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"IR signal with name '{name}' not found.")    
-        
-def include_routes(app: FastAPI, engine : Engine, client : NatureLocalAPIClient):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"IR signal with name '{name}' not found.",
+            )
+
+
+def include_routes(app: FastAPI, engine: Engine, client: NatureLocalAPIClient):
     @app.get("/nature.remo.local/{name}", response_model=IRSignal)
     async def get_ir_signal_endpoint(name: str):
         return get_ir_signal(engine, name)
-    
+
     @app.post("/nature.remo.local/{name}", status_code=status.HTTP_204_NO_CONTENT)
     async def send_ir_signal_endpoint(name: str):
         send_ir_signal(engine, name, client)
-        
+
     @app.put("/nature.remo.local/{name}", status_code=status.HTTP_201_CREATED)
-    async def create_or_update_ir_signal_endpoint(name: str, ir_signal : IRSignal):
-        return create_or_update_ir_signal(engine, name, ir_signal)    
-    
+    async def create_or_update_ir_signal_endpoint(name: str, ir_signal: IRSignal):
+        return create_or_update_ir_signal(engine, name, ir_signal)
+
     @app.delete("/nature.remo.local/{name}", status_code=status.HTTP_204_NO_CONTENT)
     async def delete_ir_signal_endpoint(name: str):
         delete_ir_signal(engine, name)
--- src/model.py
+++ src/model.py
@@ -3,6 +3,7 @@ from typing import List
 from sqlmodel import SQLModel, Field, Column
 from sqlalchemy import JSON
 
+
 class IRSignal(BaseModel):
     """
     赤外線信号を表すモデル。
@@ -13,7 +14,11 @@ class IRSignal(BaseModel):
         format (str): `data`配列内の値のフォーマットと単位。`us`に固定されており、各整数値はマイクロ秒を表します。
     """
 
-    freq: int = Field(ge=30, le=80, default=38,)
+    freq: int = Field(
+        ge=30,
+        le=80,
+        default=38,
+    )
     data: List[int] = Field(
         ...,
     )
@@ -21,12 +26,14 @@ class IRSignal(BaseModel):
         default="us",
     )
 
+
 # SQLModel モデルの定義
 class IRSignalTable(SQLModel, table=True):
     name: str = Field(default=None, primary_key=True)
     freq: int
     data: List[int] = Field(sa_column=Column(JSON))
     format: str = Field(default="us")
+
     @classmethod
     def new(cls, name: str, ir_signal: IRSignal):
         return cls(
@@ -34,14 +41,12 @@ class IRSignalTable(SQLModel, table=True):
             freq=ir_signal.freq,
             data=ir_signal.data,
             format=ir_signal.format,
-        )    
-    
-    def update(self, ir_signal : IRSignal):
+        )
+
+    def update(self, ir_signal: IRSignal):
         self.freq = ir_signal.freq
         self.data = ir_signal.data
         self.format = ir_signal.format
 
-        
     def convert_ir_signal(self) -> IRSignal:
         return IRSignal(freq=self.freq, data=self.data, format=self.format)
-    
